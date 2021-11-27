21cb458a9239b6399bc51a4ac009e0a91a92b218
simi1912 <nioasca@gmail.com>
2021-11-27 18:34:31 +0200

    Initial commit

---
+++ src/main/java/com/luv2code/springdemo/controller/CustomerController.java
@@ -0,0 +1,29 @@
+/*Code written by:
+
+	Dipl. Eng. Ioan Simiciuc
+	Software Developer
+
+	ioan.simiciuc@continental-corporation.com
+
+	Advanced driver-assistance systems (ADAS)
+	Autonomous Mobility and Safety (AMS)
+
+	S.C. Continental Automotive Romania S.R.L. Iasi
+ */
+
+package com.luv2code.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/customer")
+public class CustomerController{
+
+    @RequestMapping("/list")
+    public String listCustomers(Model model){
+        return "list-customers";
+    }
+
+}
